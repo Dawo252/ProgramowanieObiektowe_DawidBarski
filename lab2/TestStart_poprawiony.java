2876f421ca6d028be47bb7984230f462499bafd1
Dawo252 <58953717+Dawo252@users.noreply.github.com>
2020-10-15 02:01:07 +0200

    Add files via upload

---
+++ lab2/TestStart_poprawiony.java
@@ -0,0 +1,31 @@
+package pl.edu.student.dawidBa.lab2;
+
+
+public class TestStart_poprawiony {
+    public static void main(String[] args) {
+        System.out.println("----------Zadanie 1 -----------");
+        Zadanie1.A();
+        Zadanie1.B();
+        Zadanie1.C();
+        Zadanie1.D();
+        Zadanie1.E();
+        Zadanie1.F();
+        Zadanie1.G();
+        Zadanie1.H();
+        Zadanie1.I();
+        System.out.println("----------Zadanie 2 -----------");
+        Zadanie2_1.A();
+        Zadanie2_1.B();
+        Zadanie2_1.C();
+        Zadanie2_1.D();
+        Zadanie2_1.E();
+        Zadanie2_1.F();
+        Zadanie2_1.G();
+        Zadanie2_1.H();
+        Zadanie2.Zad2();
+        Zadanie2_2.Zad2_2();
+        Zadanie2_3.Zad2_3();
+        Zadanie2_4.Zad2_4();
+        Zadanie2_5.Zad2_5();
+    }
+}
